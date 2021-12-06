a0c6358b67611e3434650f0d1378791144c64c26
WHighW <murillo.ferreira200@gmail.com>
2021-12-06 19:15:48 -0300

    06-12-2021-M7
    - Coloquei banner broken control no menu.
    - Mudei cores do geometric jump.
    - Coloquei thumbnail do geometric jump.
    - Diminui volume da música do Breakout.

--- js/slide.js
+++ js/slide.js
@@ -1,55 +1,18 @@
-var sliderResponsivo = function() {
+var slideIndex = 1;
 
-var slider = document.getElementById("slider");
-var larguraSlider = slider.offsetWidth;
-var listaSlider = document.getElementById("slideWrap");
-var quant = 1;
-var items = listaSlider.querySelectorAll("li").length;
-var ant = document.getElementById("ant");
-var prox = document.getElementById("prox");
-    
-window.addEventListener('resize', function() {
-    larguraSlider = slider.offsetWidth;
-});
-    
-var slideAnterior = function() {
-    if(quant > 1) {
-    quant = quant - 2;
-    listaSlider.style.left = "-" + quant * larguraSlider + "px";
-    quant++;
-    }
-    else if(count = 1) {
-    quant = items - 1;
-    listaSlider.style.left = "-" + quant * larguraSlider + "px";
-    quant++;
-    }
-};
-    
-var proximoSlide = function() {
-    if(quant < items) {
-    listaSlider.style.left = "-" + quant * larguraSlider + "px";
-    quant++;
-    }
-    else if(quant = items) {
-    listaSlider.style.left = "0px";
-    quant = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.querySelectorAll('.slide');
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
-};
-    
-prox.addEventListener("click", function() {
-    proximoSlide();
-    });
-    
-ant.addEventListener("click", function() {
-    slideAnterior();
-});
-    
-setInterval(function() {
-  proximoSlide()
-}, 5200);
-    
-};
-    
-window.onload = function() {
-sliderResponsivo();  
+  x[slideIndex-1].style.display = "block";
 }
